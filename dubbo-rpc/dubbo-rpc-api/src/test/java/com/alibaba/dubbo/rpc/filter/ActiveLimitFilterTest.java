1454d9451f8bb7b97762baf579c7f647c8d979e3
yanghe <comicme_yanghe@icloud.com>
2016-11-02 16:53:56 +0800

    DUBBOX-1935: com.alibaba.dubbo.rpc.filter.ActiveLimitFilterTest#77: 从1秒提升到5秒

--- dubbo-rpc/dubbo-rpc-api/src/test/java/com/alibaba/dubbo/rpc/filter/ActiveLimitFilterTest.java
+++ dubbo-rpc/dubbo-rpc-api/src/test/java/com/alibaba/dubbo/rpc/filter/ActiveLimitFilterTest.java
@@ -74,7 +74,7 @@ public class ActiveLimitFilterTest {
             thread.start();
         }
         try {
-            Thread.sleep(1000);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
